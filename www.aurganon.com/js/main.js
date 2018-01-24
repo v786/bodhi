66cfd7dedf42fd693e27c9b070e30c4cba16f2ac
v786 <jahangirali786@gmail.com>
2018-01-25 00:03:27 +0530

    first

---
+++ www.aurganon.com/js/main.js
@@ -0,0 +1,211 @@
+/** 
+ * ===================================================================
+ * Main js
+ *
+ * ------------------------------------------------------------------- 
+ */ 
+
+(function($) {
+
+	"use strict";
+
+	/* --------------------------------------------------- */
+	/* Preloader
+	------------------------------------------------------ */ 
+   $(window).load(function() {
+      // will first fade out the loading animation 
+    	$("#loader").fadeOut("slow", function(){
+
+        // will fade out the whole DIV that covers the website.
+        $("#preloader").delay(300).fadeOut("slow");
+
+      }); 
+  	})
+
+
+  	/*---------------------------------------------------- */
+	/* FitVids
+	------------------------------------------------------ */ 
+  	$(".fluid-video-wrapper").fitVids();
+
+
+	/* --------------------------------------------------- */
+	/*  Vegas Slideshow
+	------------------------------------------------------ */
+    /*	$(".home-slides").vegas({
+		transition: 'fade',
+		transitionDuration: 2500,
+		delay: 5000,
+    	slides: [
+       	{ src: "https://images4.alphacoders.com/556/556449.jpg" },
+        	{ src: "https://i.pinimg.com/originals/f6/01/27/f601278d00056ff6770026da64bcc9cb.jpg" },
+        	{ src: "https://i.pinimg.com/originals/f6/01/27/f601278d00056ff6770026da64bcc9cb.jpg" }
+    	]
+	});
+
+
+	/* --------------------------------------------------- */
+	/*  Particle JS
+	------------------------------------------------------ */
+	$('.home-particles').particleground({
+	   dotColor: '#FFFFFF',
+	   //lineColor: '#FCFCFC',
+	   lineColor: '#FEE8E8',
+	   particleRadius: 25,
+	   curveLines: true,
+	   density: 10000,
+	   proximity: 200,
+      
+	}); 
+
+
+	/*-----------------------------------------------------*/
+	/* tabs
+  	-------------------------------------------------------*/	
+	$(".tab-content").hide();
+	$(".tab-content").first().show();
+
+	$("ul.tabs li").click(function () {
+	   $("ul.tabs li").removeClass("active");
+	   $(this).addClass("active");
+	   $(".tab-content").hide();
+	   var activeTab = $(this).attr("data-id");
+	   $("#" + activeTab).fadeIn(700);
+	});
+
+
+	/*----------------------------------------------------*/
+  	/* Smooth Scrolling
+  	------------------------------------------------------*/
+  	$('.smoothscroll').on('click', function (e) {
+	 	
+	 	e.preventDefault();
+
+   	var target = this.hash,
+    	$target = $(target);
+
+    	$('html, body').stop().animate({
+       	'scrollTop': $target.offset().top
+      }, 800, 'swing', function () {
+      	window.location.hash = target;
+      });
+
+  	});
+
+
+  	/* --------------------------------------------------- */
+	/*  Placeholder Plugin Settings
+	------------------------------------------------------ */
+	$('input, textarea, select').placeholder()  
+
+
+  	/*---------------------------------------------------- */
+   /* ajaxchimp
+	------------------------------------------------------ */
+
+	// Example MailChimp url: http://xxx.xxx.list-manage.com/subscribe/post?u=xxx&id=xxx
+	var mailChimpURL = 'http://facebook.us8.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e65110b38d'
+
+	$('#mc-form').ajaxChimp({
+
+		language: 'es',
+	   url: mailChimpURL
+
+	});
+
+	// Mailchimp translation
+	//
+	//  Defaults:
+	//	 'submit': 'Submitting...',
+	//  0: 'We have sent you a confirmation email',
+	//  1: 'Please enter a value',
+	//  2: 'An email address must contain a single @',
+	//  3: 'The domain portion of the email address is invalid (the portion after the @: )',
+	//  4: 'The username portion of the email address is invalid (the portion before the @: )',
+	//  5: 'This email address looks fake or invalid. Please enter a real email address'
+
+	$.ajaxChimp.translations.es = {
+	  'submit': 'Submitting...',
+	  0: '<i class="fa fa-check"></i> We have sent you a confirmation email',
+	  1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
+	  2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+	  5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
+	}
+
+
+	/*---------------------------------------------------- */
+	/*	contact form
+	------------------------------------------------------ */
+
+	/* local validation */
+	$('#contactForm').validate({
+
+		/* submit via ajax */
+		submitHandler: function(form) {
+
+			var sLoader = $('#submit-loader');
+
+			$.ajax({      	
+
+		      type: "POST",
+		      url: "inc/sendEmail.php",
+		      data: $(form).serialize(),
+		      beforeSend: function() { 
+
+		      	sLoader.fadeIn(); 
+
+		      },
+		      success: function(msg) {
+
+	            // Message was sent
+	            if (msg == 'OK') {
+	            	sLoader.fadeOut(); 
+	               $('#message-warning').hide();
+	               $('#contactForm').fadeOut();
+	               $('#message-success').fadeIn();   
+	            }
+	            // There was an error
+	            else {
+	            	sLoader.fadeOut(); 
+	               $('#message-warning').html(msg);
+		            $('#message-warning').fadeIn();
+	            }
+
+		      },
+		      error: function() {
+
+		      	sLoader.fadeOut(); 
+		      	$('#message-warning').html("Something went wrong. Please try again.");
+		         $('#message-warning').fadeIn();
+
+		      }
+
+	      });     		
+  		}
+
+	});
+
+
+	/*----------------------------------------------------*/
+	/* Final Countdown Settings
+	------------------------------------------------------ */
+	var finalDate = '2016/09/23';
+
+	$('div#counter').countdown(finalDate)
+   	.on('update.countdown', function(event) {
+
+   		$(this).html(event.strftime('<div class=\"half\">' +
+   											 '<span style="font-size:90px;">%D <sup>days</sup></span>' + 
+   										 	 '<span style="font-size:90px;">%H <sup>hours</sup></span>' + 
+   										 	 '</div>' +
+   										 	 '<div class=\"half\">' +
+   										 	 '<span style="font-size:90px;">%M <sup>mins</sup></span>' +
+   										 	 '<span style="font-size:90px;">%S <sup>secs</sup></span>' +
+   										 	 '</div>'));
+
+   });     
+ 
+
+})(jQuery);
